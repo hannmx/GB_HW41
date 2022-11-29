02d7e009997afd030877a8dad15a3e970e2b7a1e
hannmx <mxkud@mail.ru>
2022-11-30 00:26:07 +0300

    Создал программу и добавил ее на отслеживание в Git

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целые числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
